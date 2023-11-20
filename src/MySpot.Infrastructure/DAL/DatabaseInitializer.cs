d2280485348592b3bc073a45ae97e8ee04f3f84a
hczaja <hubert_czaja@o2.pl>
2023-11-20 08:58:14 +0100

    Move db migration and initialization to infrastructure hosted service

---
+++ src/MySpot.Infrastructure/DAL/DatabaseInitializer.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MySpot.Application.Services;
+using MySpot.Core.Entities;
+using MySpot.Core.ValueObjects;
+
+namespace MySpot.Infrastructure.DAL;
+
+internal sealed class DatabaseInitializer : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public DatabaseInitializer(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<MySpotDbContext>();
+            dbContext.Database.Migrate();
+
+            var weeklyParkingSpots = dbContext.WeeklyParkingSpots.ToList();
+            if (!weeklyParkingSpots.Any())
+            {
+                var clock = _serviceProvider.GetRequiredService<IClock>();
+                weeklyParkingSpots = new List<WeeklyParkingSpot>()
+                {
+                    new (Guid.Parse("00000000-0000-0000-0000-000000000001"), new Week(clock.Current()), "P1"),
+                    new (Guid.Parse("00000000-0000-0000-0000-000000000002"), new Week(clock.Current()), "P2"),
+                    new (Guid.Parse("00000000-0000-0000-0000-000000000003"), new Week(clock.Current()), "P3"),
+                    new (Guid.Parse("00000000-0000-0000-0000-000000000004"), new Week(clock.Current()), "P4"),
+                    new (Guid.Parse("00000000-0000-0000-0000-000000000005"), new Week(clock.Current()), "P5")
+                };
+
+                dbContext.WeeklyParkingSpots.AddRange(weeklyParkingSpots);
+                dbContext.SaveChanges();
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
--- src/MySpot.Infrastructure/DAL/Extensions.cs
+++ src/MySpot.Infrastructure/DAL/Extensions.cs
@@ -12,6 +12,8 @@ internal static class Extensions
         const string connectionString = "Host=localhost;Database=myspot;Username=postgres;Password=";
         services.AddDbContext<MySpotDbContext>(x => x.UseNpgsql(connectionString));
         services.AddScoped<IWeeklyParkingSpotRepository, PostgresWeeklyParkingSpotRepository>();
+        services.AddHostedService<DatabaseInitializer>();
+
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         
         return services;
